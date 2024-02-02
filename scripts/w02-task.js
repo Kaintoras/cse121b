29187e3820f44f2ddf84105ec24968ed2ad69b61
Kaintoras <josgarcia99@gmail.com>
2024-02-02 05:22:48 +0100

    Week 2 Done

---
+++ scripts/w02-task.js
@@ -0,0 +1,26 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Joseph Garcia';
+let currentYear = 2024;
+let profilePicture = "images/yo.jpg";
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of [${fullName}] `);
+/* Step 5 - Array */
+let favFood = ['Arepa', 'Lasaña', 'Pizza'];
+favFood.push('Sushi');
+foodElement.innerHTML += `<br>${favFood}`;
+favFood.shift();
+foodElement.innerHTML += `<br>${favFood}`;
+favFood.pop();
+foodElement.innerHTML += `<br>${favFood}`;
